c7e4cf45ba7d3d0c88a7cd426b510a0118c7f4e0
DratkOMG <dratkomg@gmail.com>
2023-02-01 19:52:23 +0300

    update:"

--- src/main/java/com/example/datshopspring2/securiry/config/SecurityConfig.java
+++ src/main/java/com/example/datshopspring2/securiry/config/SecurityConfig.java
@@ -1,21 +1,10 @@
 package com.example.datshopspring2.securiry.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Autowired
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
 
 }
