2cad81a31bbf27de669d9d226c91f12a12d43425
DratkOMG <dratkomg@gmail.com>
2023-02-01 20:03:31 +0300

    reload

--- src/main/java/com/example/datshopspring2/securiry/config/SecurityConfig.java
+++ src/main/java/com/example/datshopspring2/securiry/config/SecurityConfig.java
@@ -3,8 +3,7 @@ package com.example.datshopspring2.securiry.config;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
 
-
-
+    
 }
